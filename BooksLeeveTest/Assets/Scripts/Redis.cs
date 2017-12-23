dc17c8c51ad78cea3c74daddcef14ce0a1baa61f
小林怜登 <reito86chikuwa@yahoo.co.jp>
2017-12-24 05:57:31 +0900

    fbxImported

---
+++ BooksLeeveTest/Assets/Scripts/Redis.cs
@@ -0,0 +1,39 @@
+﻿
+#region Include
+using System.Threading.Tasks;
+using TeamDev.Redis;
+#endregion
+
+namespace URedis {
+    public class Redis {
+        public string ipAddress { get; set; }
+        public int port { get; set; }
+
+        private RedisDataAccessProvider client;
+
+        public async Task Connect(string ipAddress, int port=6379) {
+            await Task.Run(() => {
+                client = new RedisDataAccessProvider();
+                client.Configuration.Host = ipAddress;
+                client.Configuration.Port = port;
+                client.Connect();
+            });
+        } 
+
+        public async Task Set (string key, string value) {
+            await Task.Run(() => {
+                client.SendCommand(RedisCommand.SET, key, value);
+                client.WaitComplete();
+            }); 
+        }
+
+        public async Task<string> Get (string key) {
+            string ret = "";
+            await Task.Run(() => {
+                client.SendCommand(RedisCommand.GET, key);
+                ret = client.ReadString();
+            });
+            return ret;
+        }
+    }
+}
---
+++ BooksLeeveTest/Assets/Scripts/Sample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using URedis;
+
+public class Sample : MonoBehaviour {
+
+    [SerializeField] private string ipAddress;
+    [SerializeField] private int port = 6379;
+    [SerializeField] private string key="test";
+    [SerializeField] private string value = "hoge";
+
+    private Redis redis;
+
+    private void Start() {
+        ExampleConnect();
+    }
+
+    private void Update() {
+        // 左クリックでセット
+        if(Input.GetMouseButtonDown(0)) {
+            ExampleSet();
+
+        // 右クリックでゲット
+        } else if(Input.GetMouseButtonDown(1)) {
+            ExampleGet();
+        }
+    }
+
+    private async void ExampleSet() {
+        await redis.Set(key, value);
+        Debug.Log("Setted");
+    }
+
+    private async void ExampleGet() {
+        Task<string> getter = redis.Get(key);
+        string data = await getter;
+        Debug.Log(data);
+    }
+
+    public async void ExampleConnect() {
+        redis = new Redis();
+        await redis.Connect(ipAddress, port);
+        Debug.Log("Connected");
+    }
+}
