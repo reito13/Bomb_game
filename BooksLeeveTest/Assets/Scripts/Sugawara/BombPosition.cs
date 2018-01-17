99aa9e7304c05597eb6dd09978eb57593951d21a
Ysugashi <sugashi0713@google.com>
2018-01-17 16:19:27 +0900

    jumpの同期

---
+++ BooksLeeveTest/Assets/Scripts/Sugawara/BombPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPosition : MonoBehaviour {
+
+	private void FixedUpdate()
+	{
+		Set();
+		Get();
+	}
+
+	private async void Set()
+	{
+
+	}
+
+	private async void Get()
+	{
+
+	}
+}
