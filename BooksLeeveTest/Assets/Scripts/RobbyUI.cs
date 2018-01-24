a3642dde2cb107f3fdba2e559c43231f1f3f66c9
小林怜登 <reito86chikuwa@yahoo.co.jp>
2018-01-24 13:39:32 +0900

    RobbyUI,MatchingSystem実装途中

---
+++ BooksLeeveTest/Assets/Scripts/RobbyUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//マッチングロビー用UI
+
+public class RobbyUI : MonoBehaviour {
+
+    GameObject ButtonPrefab;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Transform list = transform.Find("List");
+        for (int i = 0; i <= 5; i++)
+        {
+            //プレハブからボタンを生成
+            GameObject listButton = Instantiate(ButtonPrefab) as GameObject;
+            //Vertical Layout Group の子にする
+            listButton.transform.SetParent(list, false);
+            listButton.transform.Find("Text").GetComponent<Text>().text = i.ToString();
+            //以下、追加---------
+            int n = i;
+            //引数に何番目のボタンかを渡す
+            listButton.GetComponent<Button>().onClick.AddListener(() => MyOnClick(n));
+        }
+    }
+    void MyOnClick(int index)
+    {
+        print(index);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
