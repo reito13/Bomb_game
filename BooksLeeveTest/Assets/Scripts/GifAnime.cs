1ac6283ea5748d51ab3c401ef06198409c76db9a
Ysugashi <sugashi0713@google.com>
2018-02-06 11:58:16 +0900

    no message

---
+++ BooksLeeveTest/Assets/Scripts/GifAnime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GifAnime : MonoBehaviour
+{
+
+    public Texture[] PlayerTexture;
+    public float gifNum = 0;
+    public float fps = 24;
+
+    void Update()
+    {
+        gifNum = Time.time * fps;
+        gifNum = gifNum % PlayerTexture.Length;
+    //    renderer.material.mainTexture = PlayerTexture[(int)gifNum];
+        Debug.Log(Mathf.Floor(gifNum));
+    }
+}
+	
